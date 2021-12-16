76e5df00da2206f485897a82ffe8c4590f792318
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2021-12-16 14:18:14 -0500

    Update resnet_model.py

--- resnet_model.py
+++ resnet_model.py
@@ -7,7 +7,7 @@
 # import tensorflow as tf
 import tensorflow.compat.v1 as tf
 tf.compat.v1.disable_eager_execution()
-tf.disable_v2_behavior(
+tf.disable_v2_behavior()
 from tensorpack.tfutils.argscope import argscope
 from tensorpack.models import (
     Conv2D, MaxPooling, AvgPooling, GlobalAvgPooling, BatchNorm, FullyConnected, BNReLU)
